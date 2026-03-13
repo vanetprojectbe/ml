d9bfea06103f076d040fa9125afec7388c574ed4
vanetprojectbe <vanetprojectbe@gmail.com>
2026-03-13 18:55:53 +0530

    Add files via upload

---
+++ ml-service/feature_schema.py
@@ -0,0 +1,18 @@
+FEATURE_ORDER = [
+    "acc_delta",
+    "gyro_delta",
+    "vibration_intensity",
+    "impact_duration",
+    "airbag_deployed",
+    "wheel_speed_drop_pct",
+    "thermal_c",
+    "latitude",
+    "longitude",
+    "initial_speed"
+]
+
+SEVERITY_MAP = {
+    0: "MINOR",
+    1: "MAJOR",
+    2: "CRITICAL"
+}
