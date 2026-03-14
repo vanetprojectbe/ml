1e78764cebd682966fbd7c33724e9a506beb7e42
vanetprojectbe <vanetprojectbe@gmail.com>
2026-03-14 19:46:16 +0530

    Update model.py

--- ml_service/model.py
+++ ml_service/model.py
@@ -1,19 +1,22 @@
 import joblib
 import numpy as np
+import os
 
 from ml_service.feature_schema import FEATURE_ORDER, SEVERITY_MAP
+
+BASE_DIR = os.path.dirname(__file__)
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
 
 class AccidentModel:
 
     def __init__(self):
-        self.model = joblib.load("model.pkl")
+        self.model = joblib.load(MODEL_PATH)
 
-    def predict(self,features):
+    def predict(self, features):
 
         X = np.array([[features[f] for f in FEATURE_ORDER]])
 
         probs = self.model.predict_proba(X)[0]
-
         cls = probs.argmax()
 
         return {
